a2f434a748ae2b95fb88e302afe5243fe023532f
hizkeel <dhizkeel@gmail.com>
2021-06-01 18:07:48 +0100

    first commit

---
+++ app/src/main/java/com/hizkeel/mapoperatoractivity/FlatMapActivity.java
@@ -0,0 +1,14 @@
+package com.hizkeel.mapoperatoractivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlatMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flat_map);
+    }
+}
